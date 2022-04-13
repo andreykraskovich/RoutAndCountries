4ae9efe783e2a4569ab8b1cbc061f802aa156859
Andrew <98891710+andreykraskovich@users.noreply.github.com>
2022-04-13 20:04:43 +0300

    Update Routing.java

--- src/main/java/modsen/alghoritm/Routing.java
+++ src/main/java/modsen/alghoritm/Routing.java
@@ -7,21 +7,27 @@ import java.util.List;
 
 public class Routing {
 
-    public boolean getRoute(Country from, Country to){
+    public List<String> getRouting(Country from, Country to){
         List<String> fromBorders = new ArrayList<String>(List.of(from.getBorders()));
         List<String> toBorders = new ArrayList<String>(List.of(to.getBorders()));
 
+        List<String> routing = new ArrayList<>();
+
         if(fromBorders.contains(to.getCode())){
-            return true;
+            routing.add(from.getCode());
+            routing.add(to.getCode());
+            return routing;
         }
         if(!fromBorders.contains(to.getCode())){
             for(int i = 0; i < fromBorders.size(); i++){
                 if(toBorders.contains(fromBorders.get(i))){
-                    return true;
+                    routing.add(from.getCode());
+                    routing.add(fromBorders.get(i));
+                    routing.add(to.getCode());
                 }
             }
         }
 
-        return false;
+        return routing;
     }
 }
